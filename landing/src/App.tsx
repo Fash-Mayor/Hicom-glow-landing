0d792186ed8d77a00d1abb82de9bd21a0d8aed09
Fash Mayor <mayowasamson03@gmail.com>
2026-03-23 13:54:43 +0100

    Added vercel web analytics

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,9 +1,11 @@
 import Landing from './landing';
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <div className="App">
       <Landing />
+      <Analytics />
     </div>
   );
 }
